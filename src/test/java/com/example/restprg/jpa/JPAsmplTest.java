4ca64f57f782a700b9323d670dc356c18e380881
yongBomb <yongBomb90@gmail.com>
2021-09-21 19:04:10 +0900

    lazy fetch

--- src/test/java/com/example/restprg/jpa/JPAsmplTest.java
+++ src/test/java/com/example/restprg/jpa/JPAsmplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 /**
@@ -22,6 +23,20 @@ public class JPAsmplTest {
     void TestConfigInit() {
         List<MemberEntity> list = memberRepo.findAll();
         System.out.println(list);
+    }
+
+
+
+    /**
+     * JPA n+1 문제 해결방안
+     * 하나의 트랜재션내에서 한번의 풀링으로 연관되어 있는 데이터를 가졍온다.
+     */
+    @Test
+    @Transactional
+    void Lazy() {
+        List<MemberEntity> list = memberRepo.findAll();
+        MemberEntity member = list.get(0);
+        System.out.println(member.getOrders());
     }
 
 
