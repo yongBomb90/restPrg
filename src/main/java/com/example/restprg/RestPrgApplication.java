606c9e2316aeb1a7db70979f80a4880ab552348e
yongBomb <79437777+yongBomb90@users.noreply.github.com>
2021-09-25 22:42:35 +0900

    Update RestPrgApplication.java

--- src/main/java/com/example/restprg/RestPrgApplication.java
+++ src/main/java/com/example/restprg/RestPrgApplication.java
@@ -3,6 +3,9 @@ package com.example.restprg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+Bomb RestPrg Sample
+**/
 @SpringBootApplication
 public class RestPrgApplication {
 
